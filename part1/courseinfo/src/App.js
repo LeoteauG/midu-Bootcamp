f7e1b2fab87e3a0a62e1ac1a4150265919663440
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-01-11 17:12:27 -0500

    change: se incluyo nuevo componente para practicar useState

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Content from "./components/Content"
 import Total from "./components/Total"
+import Clicksused from "./components/State"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header title = "'Half Stack application development'"/>
       <Content/>
       <Total title = "Number of exercises"/>
+      <Clicksused/>
     </div>
   )
 }
