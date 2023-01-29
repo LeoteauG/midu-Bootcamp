c699d1f660d25431c919db8e8203c42ad7ec1b32
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-01-28 23:38:02 -0500

    change: configurando el mongoose

--- part2/mongo.js
+++ part2/mongo.js
@@ -1,7 +1,8 @@
+/* eslint-disable new-cap */
 const password = 'Tavitols2410'
 const mongo = require('mongoose')
 const connectionString = `mongodb+srv://Gustavo:${password}@cluster0.peau9n6.mongodb.net/primerDB?retryWrites=true&w=majority`
-
+const { model, Schema } = mongo
 mongo.set('strictQuery', true)
 
 mongo.connect(connectionString)
@@ -10,4 +11,27 @@ mongo.connect(connectionString)
   })
   .catch((err) => {
     console.error(err)
+  })
+
+const noteSchema = new Schema({
+  title: String,
+  descripcion: String,
+  date: Date
+})
+
+const Nota = new model('Nota', noteSchema)
+
+const nota = new Nota({
+  title: 'Feliz Navidad',
+  descripcion: 'Para todo el mundo',
+  date: new Date()
+})
+
+nota.save()
+  .then(result => {
+    console.log(result)
+    mongo.connection.close()
+  })
+  .catch(error => {
+    console.error(error)
   })
